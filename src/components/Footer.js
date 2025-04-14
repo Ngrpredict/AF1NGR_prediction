2a19d4a2302b28f019ff3af155f6fcb1adef51aa
ngr-predictions-frontend-1 <mdquizcorner524@gmail.com>
2025-04-14 20:33:39 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,12 @@
-Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; {new Date().getFullYear()} NGR Predictions. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
