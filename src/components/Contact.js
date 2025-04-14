461cc5979097d4c81240fafc747e550eec0eef92
ngr-predictions-frontend-1 <mdquizcorner524@gmail.com>
2025-04-14 17:55:01 +0200

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React, { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+  const form = useRef();
+  const [sent, setSent] = useState(false);
+  const [error, setError] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          set
